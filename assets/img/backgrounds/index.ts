19ee770a1f5b149b077cd0cc05c0de930087324a
Osamah Max <yhohwwx4@gmail.com>
2026-01-27 14:25:16 +0300

    Updates

---
+++ assets/img/backgrounds/index.ts
@@ -0,0 +1,6 @@
+// Background images exports
+// This file exports all background images used in the application
+
+export { default as factoryHero } from './factory-hero.jpg';
+export { default as himaplasticHero } from './himaplastic_hero.jpg';
+
